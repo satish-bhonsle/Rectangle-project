00742fd6d85c480d08b59e9dd8e3c323b7a34c15
root <root@satishbhonsle1.mylabserver.com>
2018-03-24 04:36:30 +0000

    updated Rectangle class

---
+++ src/Rectangulator.java
@@ -0,0 +1,11 @@
+public class Rectangulator{
+
+	public static void main(String[] args){
+		int length = parse.Integer(args[0]);
+		int width = parse.Integer(args[1]);
+
+		Rectangle myObj = new Rectangle(leength,width);
+		system.out.println("Area of the REctangule = %d, myObj.getArea())
+	}
+
+}
